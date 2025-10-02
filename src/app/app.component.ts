07c2cd68602e2cf080ec4e178928437baa5e4f7f
jrecka <jolantua@gmail.com>
2025-10-02 13:40:02 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img src="../assets/logo.svg" alt="logo" class="brand-logo">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>`,
+  styleUrls: ['./app.component.css'],
+  imports: [
+    HomeComponent
+  ]
+})
+export class AppComponent {
+  title = 'homes';
+}
